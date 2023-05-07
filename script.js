cf5f41137d9a4f59146f83141e89f42e93359447
Yuriferr <yurifernandespreto@gmail.com>
2023-05-07 11:29:33 -0300

    Final commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const snowContainer = document.getElementById("snow-container");
+const snowContent = ['.', '.', '.']
+
+const random = (num) => {
+  return Math.floor(Math.random() * num);
+}
+
+const getRandomStyles = () => {
+  const top = random(100);
+  const left = random(100);
+  const dur = random(10) + 10;
+  const size = random(50) + 50;
+  return `
+    top: -${top}%;
+    left: ${left}%;
+    font-size: ${size}px;
+    animation-duration: ${dur}s;
+  `;
+}
+
+const createSnow = (num) => {
+  for (var i = num; i > 0; i--) {
+    var snow = document.createElement("div");
+    snow.className = "snow";
+    snow.style.cssText = getRandomStyles();
+    snow.innerHTML = snowContent[random(3)]
+    snowContainer.append(snow);
+  }
+}
+
+window.addEventListener("load", () => {
+  createSnow(100)
+});
